b9afcca15ff543bd69e85a78efeed1367b98aa50
aaronjan98 <aaronjan98@gmail.com>
2024-12-29 11:01:34 -0800

    adjust the flame randomness fxn

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /* Randomize the flame movements */
   document.querySelectorAll(".flame animateTransform").forEach((anim) => {
-    const randomRotation = Math.random() * 6 - 3; // Random between -3 and 3
+    const randomRotation = Math.random() * 8 - 4; // Random between -3 and 3
     anim.setAttribute(
       "values",
       `${randomRotation} 15 35; -${randomRotation} 15 35; ${randomRotation} 15 35`
