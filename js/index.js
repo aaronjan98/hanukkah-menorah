6f6a4701cf1366db1d865a935399f1ce82a306f3
aaronjan98 <aaronjan98@gmail.com>
2024-12-29 10:57:27 -0800

    light the corresponding candles to the day of Hanukkah

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /* Light the correct number of candles */
+  const hanukkahStart = new Date("2024-12-25");
+  const today = new Date();
+  const dayOfHanukkah =
+    Math.floor((today - hanukkahStart) / (1000 * 60 * 60 * 24)) + 1;
+
+  function lightCandles(day) {
+    console.log("day: ", day);
+    for (let i = 0; i <= 8; i++) {
+      const flame = document.querySelector(`#flame-${i}`);
+      console.log(flame);
+      if (flame) {
+        if (i <= day) {
+          flame.setAttribute("visibility", "visible");
+        } else {
+          flame.setAttribute("visibility", "hidden");
+        }
+      }
+    }
+  }
+
+  if (dayOfHanukkah >= 1 && dayOfHanukkah <= 8) {
+    lightCandles(dayOfHanukkah);
+  }
+
+  /* Randomize the flame movements */
   document.querySelectorAll(".flame animateTransform").forEach((anim) => {
     const randomRotation = Math.random() * 6 - 3; // Random between -3 and 3
     anim.setAttribute(
