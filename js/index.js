c087cc7b60d5f471462f11689e3328b679f301eb
aaronjan98 <aaronjan98@gmail.com>
2024-12-29 21:52:34 -0800

    refactor js file for better date management

--- js/index.js
+++ js/index.js
@@ -1,30 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  /* Light the correct number of candles */
+function calculateDayOfHanukkah() {
   const hanukkahStart = new Date("2024-12-25");
   const today = new Date();
-  const dayOfHanukkah =
-    Math.floor((today - hanukkahStart) / (1000 * 60 * 60 * 24)) + 1;
+  const hanukkahEnd = new Date(hanukkahStart);
+  hanukkahEnd.setDate(hanukkahEnd.getDate() + 8);
 
-  function lightCandles(day) {
-    console.log("day: ", day);
-    for (let i = 0; i <= 8; i++) {
-      const flame = document.querySelector(`#flame-${i}`);
-      console.log(flame);
-      if (flame) {
-        if (i <= day) {
-          flame.setAttribute("visibility", "visible");
-        } else {
-          flame.setAttribute("visibility", "hidden");
-        }
-      }
+  return today >= hanukkahStart && today < hanukkahEnd
+    ? Math.min(8, Math.floor((today - hanukkahStart) / (1000 * 60 * 60 * 24)))
+    : 0;
+}
+
+function lightCandles(day) {
+  for (let i = 0; i <= 8; i++) {
+    if (i > day) {
+      return;
+    }
+
+    const flame = document.querySelector(`#flame-${i}`);
+    if (flame) {
+      flame.setAttribute("visibility", "visible");
     }
   }
+}
 
-  if (dayOfHanukkah >= 1 && dayOfHanukkah <= 8) {
-    lightCandles(dayOfHanukkah);
-  }
-
-  /* Randomize the flame movements */
+function randomizeFlameMovements() {
   document.querySelectorAll(".flame animateTransform").forEach((anim) => {
     const randomRotation = Math.random() * 8 - 4; // Random between -3 and 3
     anim.setAttribute(
@@ -32,4 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
       `${randomRotation} 15 35; -${randomRotation} 15 35; ${randomRotation} 15 35`
     );
   });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dayOfHanukkah = calculateDayOfHanukkah();
+  lightCandles(dayOfHanukkah);
+  randomizeFlameMovements();
 });
